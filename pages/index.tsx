a1045469578d2b8cc0e84d822c3f11d19cfe5612
Sander <sandervanast1@gmail.com>
2022-04-08 22:27:25 +0200

    test: :boom: Check if prettier works
    checking pre-commit changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import PageContainer from '@/layout/Main';
-import useUser from '@/lib/useUser';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
@@ -7,7 +6,13 @@ const Home: NextPage = () => {
   // const { user, mutateUser } = useUser();
   // when user is logged in || redirectIfFound -> login page = true home page = false
   // const { user, mutateUser } = useUser({redirectIfFound: true, redirectTo: '/dashboard'});
-  return <PageContainer>Home</PageContainer>;
+  return <PageContainer
+  
+  
+  >Home
+  
+  
+  </PageContainer>;
 };
 
 export default Home;
